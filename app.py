2a73e038bcf048411535b073acdb7f9833c929cb
Yael Badillo <yael.badillos@gmail.com>
2024-09-20 22:11:29 -0600

    feat: add prompt response endpoint

--- app.py
+++ app.py
@@ -1,8 +1,27 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+from db_connection import get_collection
+
+
+class PromptDto(BaseModel):
+    prompt: str
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {"Hello": "World"}
+@app.post("/api/prompt-response/")
+def create_prompt_response(prompt_dto: PromptDto):
+    prompt_collection = get_collection("prompt_response")
+
+    prompt_response = {
+        "prompt": prompt_dto.prompt,
+        "response": "",
+        "created_at": datetime.now(),
+        "updated_at": datetime.now()
+    }
+
+    prompt_collection.insert_one(prompt_response)
+
+    return {"response": ""}
+
