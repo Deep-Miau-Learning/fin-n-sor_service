8819610359674901500d8069265bd96bc33a33ab
Yael Badillo <yael.badillos@gmail.com>
2024-09-20 22:27:54 -0600

    refactor: correct entities creation

--- app.py
+++ app.py
@@ -30,11 +30,11 @@ def create_prompt_response(prompt_dto: PromptDto):
     prompt_collection = get_collection("prompt_response")
 
     prompt_response = PromptResponseEntity(
-        user_id: "1",
-        prompt: prompt_dto.prompt,
-        response: "",
-        created_at: datetime.now(),
-        updated_at: datetime.now()
+        "1", 
+        prompt_dto.prompt,
+        "",
+        datetime.now(),
+        datetime.now()
     )
 
     prompt_collection.insert_one(prompt_response)
@@ -49,10 +49,10 @@ def get_financial_profile(user_id: str):
     prompt_responses = prompt_response_collection.find({"user_id": user_id})
 
     financial_profile = FinancialProfileEntity(
-        user_id: user_id,
-        output: "",
-        created_at: datetime.now(),
-        updated_at: datetime.now()
+        user_id,
+        "",
+        datetime.now(),
+        datetime.now()
     )
 
     finalcial_profile_collection.insert_one(financial_profile)
